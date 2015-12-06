3cb6d4059337fa0bd3ef8e18110785f9aa18c58d
yuxueping <cbeigong@163.com>
2015-12-07 01:44:42 +0800

    change

---
+++ jQuery拉开关闭帷幕--生日快乐！--柯乐义_files/gggg728x90a.js
@@ -0,0 +1,7 @@
+﻿<!--
+google_ad_client = "ca-pub-7740261255677392";
+google_ad_slot = "8839078091";
+google_ad_width = 728;
+google_ad_height = 90;
+document.write('<scr'+'ipt type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></sc'+'ript>');
+//-->
---
+++ jQuery拉开关闭帷幕--生日快乐！--柯乐义_files/keleyi-curtain.js
@@ -0,0 +1,25 @@
+﻿/*
+* http://keleyi.com
+* 柯乐义
+*/
+$(document).ready(function () {
+
+    $curtainopen = false;
+
+    $(".rope").click(function () {
+        $(this).blur();
+        if ($curtainopen == false) {
+            $(this).stop().animate({ top: '0px' }, { queue: false, duration: 350, easing: 'easeOutBounce' });
+            $(".leftcurtain").stop().animate({ width: '60px' }, 2000);
+            $(".rightcurtain").stop().animate({ width: '60px' }, 2000);
+            $curtainopen = true;
+        } else {
+            $(this).stop().animate({ top: '-40px' }, { queue: false, duration: 350, easing: 'easeOutBounce' });
+            $(".leftcurtain").stop().animate({ width: '50%' }, 2000);
+            $(".rightcurtain").stop().animate({ width: '51%' }, 2000);
+            $curtainopen = false;
+        }
+        return false;
+    });
+
+});	
